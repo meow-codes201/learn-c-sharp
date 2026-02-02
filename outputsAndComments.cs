b6e75be741a2fe81384852a162a960f3cd2c9557
code web <humairat291@gmail.com>
2026-02-02 22:52:42 +0530

    Add HelloWorld program with output and comments

---
+++ outputsAndComments.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello there,learner!");
+        Console.Write("\"C# is fun!\"\n");
+        Console.Write("Well.... look at this");
+        //look at this comment
+        /*is it in output
+        :D Hello there, learner! 
+        Comments are ignored*/
+        
+        
+        
+    }
+}
